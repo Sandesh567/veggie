007c0c32f09c7be08bc17f68411590a9314d1d23
Sandesh Raj Bhatta <106328473+Sandesh567@users.noreply.github.com>
2025-01-29 14:20:46 +0545

    Update Product.js

--- Product.js
+++ Product.js
@@ -24,7 +24,7 @@ export default function Product(props) {
                     shadowColor: fruit.shadow,
                     shadowRadius: 50,
                     shadowOffset: { width: 0, height: 50 },
-                    shadowOpacity: 0.7
+                    shadowOpacity: 0.9
                 }}>
                     <Image source={fruit.image} style={{ width: 290, height: 290 }} />
                 </View>
@@ -57,11 +57,13 @@ export default function Product(props) {
                 </View>
                 <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
                     <Text style={{ fontSize: 18 }}>${fruit.price}</Text>
-                    <TouchableOpacity style={{
-                        backgroundColor: fruit.shadow, opacity: 0.6, shadowColor: fruit.shadow,
-                        shadowRadius: 25, shadowOffset: { width: 0, height: 15 }, shadowOpacity: 0.9,
-                        borderRadius: 70, flex: 1, padding: 3, marginLeft: 6
-                    }}>
+                    <TouchableOpacity
+                        onPress={() => navigation.navigate('Cart')}
+                        style={{
+                            backgroundColor: fruit.shadow, opacity: 0.6, shadowColor: fruit.shadow,
+                            shadowRadius: 25, shadowOffset: { width: 0, height: 15 }, shadowOpacity: 0.9,
+                            borderRadius: 70, flex: 1, padding: 3, marginLeft: 6
+                        }}>
                         <Text style={{ fontSize: 21, color: 'white', fontWeight: 'bold' }}>
                             Add To Cart
                         </Text>
@@ -72,3 +74,41 @@ export default function Product(props) {
         </View>
     )
 }
+
+
+/*00-1
+import { Text, TouchableOpacity, View, Image } from "react-native";
+import React from "react";
+import { useNavigation } from "@react-navigation/native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { ChevronLeftIcon } from "react-native-heroicons/solid";
+
+export default function Product(props) {
+    let fruit = props.route.params;
+    const navigation = useNavigation();
+    return (
+        <View style={{ display: "flex", flex: 1, backgroundColor: fruit.color }}>
+            <SafeAreaView>
+                <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', marginTop: 5 }}>
+                    <TouchableOpacity
+                        style={{ backgroundColor: 'rgba(255,255,255,0.2', borderColor: 'grey', borderRadius: 60 }}
+                        onPress={() => navigation.goBack()}>
+                        <ChevronLeftIcon size="30" color="white" />
+                    </TouchableOpacity>
+                </View>
+                <View style={{
+                    display: 'flex', flexDirection: 'row',
+                    justifyContent: 'center', marginTop: 5, paddingBottom: 10,
+                    shadowColor: fruit.shadow,
+                    shadowRadius: 50,
+                    shadowOffset: { width: 0, height: 50 },
+                    shadowOpacity: 0.7
+                }}>
+                    <Image source={fruit.image} style={{ width: 290, height: 290 }} />
+
+                </View>
+            </SafeAreaView>
+        </View>
+    )
+}
+*/
