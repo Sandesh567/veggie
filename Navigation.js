c8ce51eb25812ccfeb40df2afabb36dba586e12f
Sandesh Raj Bhatta <106328473+Sandesh567@users.noreply.github.com>
2025-01-29 14:24:03 +0545

    Cart Screen is added to the navigation.

--- Navigation.js
+++ Navigation.js
@@ -2,6 +2,7 @@ import HomeScreen from "../screens/HomeScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Product from "../screens/Product";
+import CartScreen from "../screens/CartScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -12,6 +13,7 @@ export default function Navigation() {
             <Stack.Navigator initialRouteName='Home'>
                 <Stack.Screen name="Home" options={{ headerShown: false }} component={HomeScreen} />
                 <Stack.Screen name="Product" options={{ headerShown: false }} component={Product} />
+                <Stack.Screen name="Cart" options={{ headerShown: false }} component={CartScreen} />
             </Stack.Navigator>
         </NavigationContainer>
     )
