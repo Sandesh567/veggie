57f0f903c84138cd15cefea1bbdf1b10fadcdc30
Sandesh Raj Bhatta <106328473+Sandesh567@users.noreply.github.com>
2025-01-29 14:22:07 +0545

    Update index.js
    CartItems object is added.

--- index.js
+++ index.js
@@ -59,6 +59,55 @@ export const featuredFruits = [
 ];
 
 
+export const cartItems = [
+    {
+        name: 'Australian Orange',
+        price: "12.30",
+        stars: 4,
+        desc: 'Sweet and juicy',
+        shadow: 'orange',
+        image: require('./assets/images/orange.png'),
+        color: opacity => `rgba(251, 216, 146, ${opacity})`, // color is a function
+    },
+    {
+        name: 'Flesh Nectarine',
+        shadow: 'red',
+        price: "12",
+        stars: 3,
+        desc: 'Sweet and juicy',
+        image: require('./assets/images/peach.png'),
+        color: opacity => `rgba(255, 170, 157, ${opacity})`
+    },
+    {
+        name: 'Red Grapefruit',
+        price: "30",
+        stars: 4,
+        desc: 'Sweet and juicy',
+        shadow: 'red',
+        image: require('./assets/images/redOrange.png'),
+        color: opacity => `rgba(255, 163, 120, ${opacity})`
+    },
+    {
+        name: 'Green Apple',
+        price: "10.5",
+        stars: 4,
+        desc: 'Sweet and juicy',
+        shadow: 'green',
+        image: require('./assets/images/greenApple.png'),
+        color: opacity => `rgba(139, 243, 139, ${opacity})`
+    },
+    {
+        name: 'Black Grapes',
+        price: "40",
+        stars: 4,
+        desc: 'Sweet and juicy',
+        shadow: 'purple',
+        image: require('./assets/images/blackGrapes.png'),
+        color: opacity => `rgba(255, 163, 120, ${opacity})`
+    },
+];
+
+
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
